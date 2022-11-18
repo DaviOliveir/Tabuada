c7ea80a13b6c0b885d55b8dff63ba9ca0d5e8b86
Davi José Batista Lima de Oliveira <davibatistalimadeoliveira@gmail.com>
2022-11-18 20:25:15 -0300

    concluido

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+Console.WriteLine("Tabuada Do:  ");
+
+int Numero = Convert.ToInt32(Console.ReadLine());
+
+int Multiplo =0;
+Console.WriteLine();
+int Tabuada;
+
+while (Multiplo<=10 )
+
+{
+    Tabuada = Numero * Multiplo;
+   
+Console.WriteLine();
+
+Console.WriteLine($"{Numero} x {Multiplo} = {Tabuada} ");
+ Multiplo++;
+
+
+}
+Console.WriteLine("Obrigado por utilizar o programa :)");
+
+
+
